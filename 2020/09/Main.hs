ed2d16e28f70f47e70bb02dbdb2aa80f6b650ffd
Einar Norðfjörð <einar@nordfjord.io>
2020-12-11 23:02:34 -0500

    Day 9

--- 2020/09/Main.hs
+++ 2020/09/Main.hs
@@ -2,16 +2,40 @@
 module Main where
 
 import qualified Data.Text.IO                  as T
-import           Data.Text                      ( Text )
 import qualified Data.Attoparsec.Text          as P
+import           Data.Either                    ( fromRight )
+import           Control.Applicative            ( liftA2 )
+import           Control.Monad                  ( guard )
+import           Data.List.Split                ( divvy )
+import           System.Environment             ( lookupEnv )
 
-preamble n = take n
+part1 :: [Int] -> [Int] -> Int
+part1 _        []       = 0
+part1 preamble (n : ns) = if n `notElem` sums
+  then n
+  else part1 (tail preamble ++ [n]) ns
+  where sums = liftA2 (+) preamble preamble
+
+part2 :: Int -> [Int] -> [Int]
+part2 target ns = do
+  cat  <- [2 .. length ns]
+  list <- divvy cat 1 ns
+  guard $ sum list == target
+  return $ maximum list + minimum list
 
 inputP :: P.Parser [Int]
 inputP = P.decimal `P.sepBy` "\n"
 
 main :: IO ()
 main = do
-  contents <- P.parseOnly inputP <$> T.getContents
+  contents     <- T.getContents
+  preambleSize <- maybe 25 read <$> lookupEnv "PREAMBLE_SIZE"
+  let ns     = fromRight [] $ P.parseOnly inputP contents
 
-  print $ contents
+  let target = part1 (take preambleSize ns) (drop preambleSize ns)
+
+  putStrLn "Part 1:"
+  print target
+
+  putStrLn "Part 1:"
+  print $ part2 target ns
