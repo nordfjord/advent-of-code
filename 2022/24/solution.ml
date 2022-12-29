cf45a1f1957d7f0ff37934bacb607be685df005a
Einar Norðfjörð <einar@nordfjord.io>
2022-12-28 22:29:44 -0500

    day 24 and 25p1

---
+++ 2022/24/solution.ml
@@ -0,0 +1,90 @@
+open Printf
+
+let lines =
+  Seq.of_dispenser (fun _ ->
+      match read_line () with x -> Some x | exception End_of_file -> None)
+  |> Array.of_seq
+
+type point = int * int
+
+module PointSet = Set.Make (struct
+  type t = point
+
+  let compare = compare
+end)
+
+let winds =
+  lines |> Array.to_seq
+  |> Seq.mapi (fun row line ->
+         line |> String.to_seqi
+         |> Seq.filter_map (fun (col, c) ->
+                match c with
+                | '>' -> Some ((row, col), (0, 1))
+                | '<' -> Some ((row, col), (0, -1))
+                | '^' -> Some ((row, col), (-1, 0))
+                | 'v' -> Some ((row, col), (1, 0))
+                | _ -> None))
+  |> Seq.concat
+  |> Seq.map (fun ((row, col), vec) -> ((row - 1, col - 1), vec))
+  |> List.of_seq
+
+let rows = Array.length lines - 2
+let cols = String.length lines.(0) - 2
+let ( % ) n m = ((n mod m) + m) mod m
+
+let advance_wind ((row, col), (delta_rows, delta_cols)) =
+  ( ((row + delta_rows) % rows, (col + delta_cols) % cols),
+    (delta_rows, delta_cols) )
+
+let wind_states =
+  Seq.unfold
+    (fun prev_winds ->
+      let next = List.map advance_wind prev_winds in
+      if next = winds then None else Some (next, next))
+    winds
+  |> Seq.append (Seq.return winds)
+  |> Seq.map (List.map fst)
+  |> Seq.map PointSet.of_list |> Array.of_seq
+
+let wind_cycle = Array.length wind_states
+
+let adjacent_and_self (row, col) =
+  [ (row + 1, col); (row - 1, col); (row, col + 1); (row, col - 1); (row, col) ]
+
+let is_in_bounds (row, col) =
+  (row, col) = (rows, cols - 1)
+  || (row, col) = (-1, 0)
+  || (0 <= row && row < rows && 0 <= col && col < cols)
+
+let possible_moves (row, col) winds =
+  adjacent_and_self (row, col)
+  |> List.filter (fun p -> is_in_bounds p && not (PointSet.mem p winds))
+
+let bfs pos goal initial_minute =
+  let next_winds m = wind_states.((m + 1 + initial_minute) mod wind_cycle) in
+  let q = Queue.create () in
+  Queue.push (0, pos) q;
+  let result = ref 0 in
+  let seen_states = Hashtbl.create 100000 in
+  while (not (Queue.is_empty q)) && !result = 0 do
+    let mins, pos = Queue.take q in
+    if pos = goal then (
+      result := mins;
+      Queue.clear q)
+    else if Hashtbl.mem seen_states (mins mod wind_cycle, pos) then ()
+    else (
+      Hashtbl.add seen_states (mins mod wind_cycle, pos) ();
+      let next_positions = possible_moves pos (next_winds mins) in
+
+      next_positions |> List.iter (fun p -> Queue.add (mins + 1, p) q))
+  done;
+  !result
+
+let () =
+  let goal = (rows, cols - 1) in
+  let start = (-1, 0) in
+  let result = bfs start goal 0 in
+  printf "Part 1: %d\n" result;
+  let snack_trip = bfs goal start result in
+  let trip_back = bfs start goal (result + snack_trip) in
+  printf "Part 2: %d\n" (result + snack_trip + trip_back)
---
+++ 2022/25/solution.ml
@@ -0,0 +1,71 @@
+open Printf
+
+let snafu_char_to_int = function
+  | '=' -> -2
+  | '-' -> -1
+  | '0' -> 0
+  | '1' -> 1
+  | '2' -> 2
+  | _ -> failwith "Illegal SNAFU char"
+
+let int_to_snafu_char = function
+  | -2 -> "="
+  | -1 -> "-"
+  | 0 -> "0"
+  | 1 -> "1"
+  | 2 -> "2"
+  | _ -> failwith "Illegal SNAFU char"
+
+(* OCaml doesn't  have a pow function in the stdlib *)
+let rec ( ** ) a = function
+  | 0 -> 1
+  | 1 -> a
+  | n ->
+      let b = a ** (n / 2) in
+      b * b * if n mod 2 = 0 then 1 else a
+
+let snafu_to_int str =
+  let len = String.length str in
+  let result = ref 0 in
+  for i = 0 to len - 1 do
+    let c = snafu_char_to_int str.[len - i - 1] in
+    result := !result + ((5 ** i) * c)
+  done;
+  !result
+
+
+let int_to_snafu n =
+    let rec loop s = function
+    | 0 -> s
+    | n ->
+        let r = ((n + 2) mod 5) in
+        let d = (n + 2) / 5 in
+        loop (int_to_snafu_char r ^ s) d
+    in
+    loop "" n
+
+
+let int_to_snafu value =
+  let result = ref value in
+  let s = ref "" in
+  while !result <> 0 do
+    s := int_to_snafu_char (((!result + 2) mod 5 )- 2) ^ !s;
+    result := (!result - (((!result + 2) mod 5) - 2)) / 5
+  done;!s
+
+
+
+
+let () =
+  printf "Sanity checks\n";
+  ["1121-1110-1=0"]
+    |> List.iter (fun s ->
+
+  printf "%s = %d = %s\n" s (snafu_to_int s) (int_to_snafu (snafu_to_int s))
+        )
+
+let lines =
+  Seq.of_dispenser (fun _ ->
+      match read_line () with x -> Some x | exception End_of_file -> None)
+  |> Seq.map snafu_to_int |> Seq.fold_left ( + ) 0 |> int_to_snafu
+  |> printf "Part 1: %s\n"
