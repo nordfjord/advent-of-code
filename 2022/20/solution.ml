03d8b760c869d515925dae39b72290f24493415b
Einar Norðfjörð <einar@nordfjord.io>
2022-12-29 18:52:47 -0500

    day 20 ocaml

--- 2022/20/solution.ml
+++ 2022/20/solution.ml
@@ -5,94 +5,66 @@ let numbers =
       match read_line () with x -> Some x | exception End_of_file -> None )
   |> Seq.map int_of_string |> Array.of_seq
 
-let modulo a b = ((a mod b) + b) mod b
-
-let find_index f arr =
-  let rec aux i = if f arr.(i) then i else aux (i + 1) in
-  aux 0
-
-let show_array arr =
-  printf "[%s]\n"
-    ( Array.map snd arr |> Array.map string_of_int |> Array.to_list
-    |> String.concat ", " )
-
-let mix numbers result =
-  let len = Array.length result in
-  let simple_swap arr i j =
-    let tmp = arr.(i) in
-    arr.(i) <- arr.(j) ;
-    arr.(j) <- tmp
+let score (arr : int array) =
+  let l = Array.length arr in
+  let zi =
+    arr |> Array.to_seqi
+    |> Seq.find_map (fun (i, v) -> if v = 0 then Some i else None)
+    |> Option.get
   in
-  let swap arr i j =
-    let len = Array.length arr in
-    if i = 0 && j = -1 then
-      for i = 0 to len - 2 do
-        simple_swap arr i (i + 1)
-      done
-    else if i = len - 1 && j = len then
-      for i = len - 1 downto 1 do
-        simple_swap arr i (i - 1)
-      done
-    else
-      let i = modulo i len in
-      let j = modulo j len in
-      simple_swap arr i j
-  in
-  for i = 0 to len - 1 do
-    let value = numbers.(i) in
-    let from_idx = find_index (fun (j, _) -> j = i) result in
-    let to_idx = from_idx + (value mod (len - 1)) in
-    printf "Moving %d from %d to %d\n" value from_idx to_idx ;
-    ( if value = 0 then ()
-    else
-      let current_idx = ref from_idx in
-      let sign = if value < 0 then -1 else 1 in
-      if value < 0 then
-        for _ = value to 0 do
-          swap result !current_idx (!current_idx + sign) ;
-          current_idx := !current_idx + sign
-        done
-      else
-        for _ = 1 to value do
-          swap result !current_idx (!current_idx + sign) ;
-          current_idx := !current_idx + 1
-        done ) ;
-    show_array result
-  done
+  arr.((zi + 1000) mod l) + arr.((zi + 2000) mod l) + arr.((zi + 3000) mod l)
+
+let remove_at arr i =
+  let len = Array.length arr in
+  let left = Array.sub arr 0 i in
+  let right = Array.sub arr ((i + 1) mod len) (len - i - 1) in
+  Array.append left right
+
+let insert_at arr i value =
+  let len = Array.length arr in
+  let left = Array.sub arr 0 i in
+  let right = Array.sub arr i (len - i) in
+  Array.concat [left; [|value|]; right]
+
+type x = (int * int) array [@@deriving show]
+
+let switch arr i item =
+  let removed = remove_at arr i in
+  let l = Array.length removed - 1 in
+  let v, _ = item in
+  let n = (i + v) mod l in
+  let ni = if n <= 0 then l + n else n in
+  insert_at removed ni item
+
+let mix original arr =
+  original
+  |> Array.fold_left
+       (fun res num ->
+         let i =
+           res |> Array.to_seqi
+           |> Seq.find_map (fun (i, value) ->
+                  if value = num then Some i else None )
+           |> Option.get
+         in
+         let arr = switch res i num in
+         arr )
+       arr
+
+let shuffle numbers rounds encrypt_key =
+  let arr = numbers |> Array.mapi (fun i l -> (l * encrypt_key, i)) in
+  let original = Array.copy arr in
+  Seq.ints 0 |> Seq.take rounds
+  |> Seq.fold_left (fun arr _ -> mix original arr) arr
+  |> Array.map fst |> score
 
 (* Maybe we can swap repeatedly to move elements in the array *)
 
 let part1 () =
   printf "\n" ;
-  let result = numbers |> Array.mapi (fun i x -> (i, x)) in
-  mix numbers result ;
-  let len = Array.length result in
-  let zero_idx = find_index (fun (_, v) -> v = 0) result in
-  let x = snd result.((zero_idx + 1000) mod len) in
-  let y = snd result.((zero_idx + 2000) mod len) in
-  let z = snd result.((zero_idx + 3000) mod len) in
-  let sum = x + y + z in
-  printf "x=%d; y=%d; z=%d\n" x y z ;
-  printf "Part 1: %d\n%!" sum
+  shuffle numbers 1 1 |> printf "Part 1: %d\n"
 
 let part2 () =
   let key = 811589153 in
-  let numbers = numbers |> Array.map (( * ) key) in
-  let result = numbers |> Array.mapi (fun i x -> (i, x)) in
-  show_array result ;
-  for i = 1 to 10 do
-    mix numbers result ;
-    printf "\nAfter %d round of mixing\n" i ;
-    show_array result ;
-    printf "\n"
-  done ;
-  let len = Array.length result in
-  let zero_idx = find_index (fun (_, v) -> v = 0) result in
-  let x = snd result.((zero_idx + 1000) mod len) in
-  let y = snd result.((zero_idx + 2000) mod len) in
-  let z = snd result.((zero_idx + 3000) mod len) in
-  let sum = x + y + z in
-  printf "x=%d; y=%d; z=%d\n" x y z ;
-  printf "Part 2: %d\n" sum
+  shuffle numbers 10 key |> printf "Part 2: %d\n"
 
 let () = part1 () ; part2 ()
