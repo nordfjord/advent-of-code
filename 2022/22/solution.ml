7befd62b71770f4c8050abaa91c99c4a6595a175
Einar Norðfjörð <einar@nordfjord.io>
2022-12-29 01:19:29 -0500

    clean it up

--- 2022/22/solution.ml
+++ 2022/22/solution.ml
@@ -1,73 +1,14 @@
 open Printf
 
+type block = Air | Floor | Wall
+type move = Move of int | RotateRight | RotateLeft
+type heading = Up | Down | Left | Right [@@deriving show]
+
+let int_of_heading = function Right -> 0 | Down -> 1 | Left -> 2 | Up -> 3
+
 let lines =
   Seq.of_dispenser (fun _ ->
       match read_line () with x -> Some x | exception End_of_file -> None)
-
-type block = Air | Floor | Wall
-
-let parse_block = function
-  | ' ' -> Air
-  | '#' -> Wall
-  | '.' -> Floor
-  | _ -> failwith "Nope"
-
-let raw_map = lines |> Seq.take_while (fun l -> l <> "") |> Array.of_seq
-
-let rows, cols =
-  ( Array.length raw_map,
-    raw_map |> Array.map String.length |> Array.fold_left max 0 )
-
-let map =
-  raw_map
-  |> Array.map (fun s ->
-         let len = String.length s in
-         if len < cols then s ^ String.make (cols - len) ' ' else s)
-  |> Array.map (fun s -> String.to_seq s |> Seq.map parse_block |> Array.of_seq)
-
-let starting_position =
-  let pos = ref 0 in
-  while map.(0).(!pos) <> Floor do
-    pos := !pos + 1
-  done;
-  (0, !pos)
-
-type heading = Up | Down | Left | Right [@@deriving show]
-type move = Move of int | RotateRight | RotateLeft
-
-let rotate_r = function
-  | Up -> Right
-  | Right -> Down
-  | Down -> Left
-  | Left -> Up
-
-let rotate_l = function
-  | Up -> Left
-  | Left -> Down
-  | Down -> Right
-  | Right -> Up
-
-let modulo n m = ((n mod m) + m) mod m
-
-let move (row, col) = function
-  | Up -> (modulo (row - 1) rows, col)
-  | Down -> (modulo (row + 1) rows, col)
-  | Left -> (row, modulo (col - 1) cols)
-  | Right -> (row, modulo (col + 1) cols)
-
-let position map (row, col) = map.(row).(col)
-
-let rec advance map pos heading n =
-  if n = 0 then pos
-  else
-    let new_pos = ref (move pos heading) in
-    while position map !new_pos = Air do
-      new_pos := move !new_pos heading
-    done;
-    match position map !new_pos with
-    | Air -> failwith "Impossible Air"
-    | Wall -> pos
-    | Floor -> advance map !new_pos heading (n - 1)
 
 let matched_group_opt n s =
   match Str.matched_group n s with x -> Some x | exception Not_found -> None
@@ -89,14 +30,71 @@ let parse_input line =
   done;
   !result
 
-let score row col heading =
-  (1000 * (row + 1))
-  + (4 * (col + 1))
-  + match heading with Right -> 0 | Down -> 1 | Left -> 2 | Up -> 3
-
 let moves = parse_input (read_line ())
 
+let parse_block = function
+  | ' ' -> Air
+  | '#' -> Wall
+  | '.' -> Floor
+  | _ -> failwith "Nope"
+
+let raw_map = lines |> Seq.take_while (fun l -> l <> "") |> Array.of_seq
+
+let rows, cols =
+  ( Array.length raw_map,
+    raw_map |> Array.map String.length |> Array.fold_left max 0 )
+
+let map =
+  raw_map
+  |> Array.map (fun s ->
+         let len = String.length s in
+         if len < cols then s ^ String.make (cols - len) ' ' else s)
+
+let rotate_r = function
+  | Up -> Right
+  | Right -> Down
+  | Down -> Left
+  | Left -> Up
+
+let rotate_l = function
+  | Up -> Left
+  | Left -> Down
+  | Down -> Right
+  | Right -> Up
+
+let ( % ) n m = ((n mod m) + m) mod m
+
+let move (row, col) = function
+  | Up -> ((row - 1) % rows, col)
+  | Down -> ((row + 1) % rows, col)
+  | Left -> (row, (col - 1) % cols)
+  | Right -> (row, (col + 1) % cols)
+
+let position map (row, col) = map.(row).[col] |> parse_block
+
+let rec advance map pos heading n =
+  if n = 0 then pos
+  else
+    let new_pos = ref (move pos heading) in
+    while position map !new_pos = Air do
+      new_pos := move !new_pos heading
+    done;
+    match position map !new_pos with
+    | Air -> failwith "Impossible Air"
+    | Wall -> pos
+    | Floor -> advance map !new_pos heading (n - 1)
+
+let score row col heading =
+  (1000 * (row + 1)) + (4 * (col + 1)) + int_of_heading heading
+
 let () =
+  let starting_position =
+    let pos = ref 0 in
+    while map.(0).[!pos] <> '.' do
+      pos := !pos + 1
+    done;
+    (0, !pos)
+  in
   let (row, col), heading =
     moves
     |> Array.fold_left
@@ -112,14 +110,19 @@ let () =
   printf "row=%d; col=%d; heading=%s\n" row col (show_heading heading);
   printf "Part 1: %d\n" (score row col heading)
 
+(* Part two is different enough from part 1 that a lot of the functions have been reimplemented
+   OCaml allows shadowing so it's absolutely fine *)
+
+(* In this part we treat the players position as a 4-tuple of face, row, col, direction *)
+
+type position = int * int * int * heading
+
 let move (face, row, col, dir) =
   match dir with
   | Up -> (face, row - 1, col, dir)
   | Down -> (face, row + 1, col, dir)
   | Left -> (face, row, col - 1, dir)
   | Right -> (face, row, col + 1, dir)
-
-type position = int * int * int * heading [@@deriving show]
 
 let position map (face, row, col, _) = map.(face).(row).[col] |> parse_block
 
@@ -163,25 +166,25 @@ let rec advance map pos n =
   else
     let new_pos = move pos |> side_mapping in
     match position map new_pos with
-    | Air -> failwith ("Impossible Air at " ^ show_position new_pos)
+    | Air -> failwith "Impossible Air"
     | Wall -> pos
     | Floor -> advance map new_pos (n - 1)
 
 let get_all_sides () =
-  let one_and_two = Array.sub raw_map 0 50 in
-  let three = Array.sub raw_map 50 50 in
-  let four_and_five = Array.sub raw_map 100 50 in
-  let six = Array.sub raw_map 150 50 in
-  let one = one_and_two |> Array.map (fun s -> String.sub s 50 50) in
-  let two = one_and_two |> Array.map (fun s -> String.sub s 100 50) in
-  let three = three |> Array.map (fun s -> String.sub s 50 50) in
-  let four = four_and_five |> Array.map (fun s -> String.sub s 0 50) in
-  let five = four_and_five |> Array.map (fun s -> String.sub s 50 50) in
-  let six = six |> Array.map (fun s -> String.sub s 0 50) in
-  [| one; two; three; four; five; six |]
+  let zero_and_one = Array.sub raw_map 0 50 in
+  let two = Array.sub raw_map 50 50 in
+  let three_and_four = Array.sub raw_map 100 50 in
+  let five = Array.sub raw_map 150 50 in
+  let zero = zero_and_one |> Array.map (fun s -> String.sub s 50 50) in
+  let one = zero_and_one |> Array.map (fun s -> String.sub s 100 50) in
+  let two = two |> Array.map (fun s -> String.sub s 50 50) in
+  let three = three_and_four |> Array.map (fun s -> String.sub s 0 50) in
+  let four = three_and_four |> Array.map (fun s -> String.sub s 50 50) in
+  let five = five |> Array.map (fun s -> String.sub s 0 50) in
+  [| zero; one; two; three; four; five |]
 
 let score face row col heading =
-  let drow, dcol =
+  let dx, dy =
     match face with
     | 0 -> (0, 50)
     | 1 -> (0, 100)
@@ -191,7 +194,7 @@ let score face row col heading =
     | 5 -> (150, 0)
     | _ -> failwith ("Unexpected side " ^ string_of_int face)
   in
-  score (row + drow) (col + dcol) heading
+  (1000 * (row + dx + 1)) + (4 * (col + dy + 1)) + int_of_heading heading
 
 let () =
   let map = get_all_sides () in
