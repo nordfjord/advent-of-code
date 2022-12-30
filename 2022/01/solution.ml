afd46eff26437a93225e1a58709487fddf7f6a6b
Einar Norðfjörð <einar@nordfjord.io>
2022-12-29 20:46:14 -0500

    Day 1 OCaml

---
+++ 2022/01/solution.ml
@@ -0,0 +1,20 @@
+open Printf
+
+let numbers =
+  Seq.of_dispenser (fun _ ->
+      match read_line () with x -> Some x | exception End_of_file -> None )
+  |> Seq.fold_left
+       (fun state line ->
+         match line with
+         | "" ->
+             0 :: state
+         | s ->
+             (int_of_string s + List.hd state) :: List.tl state )
+       [0]
+
+let desc a b = -compare a b
+
+let () =
+  numbers |> List.fold_left max 0 |> printf "Part 1: %d\n" ;
+  numbers |> List.sort desc |> List.to_seq |> Seq.take 3
+  |> Seq.fold_left ( + ) 0 |> printf "Part 2: %d\n"
