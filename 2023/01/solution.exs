fc29fccd9214babad570083065cef6777d2e6ea4
Einar Norðfjörð <gloogankle@gmail.com>
2023-12-02 16:24:38 -0500

    use exs

--- 2023/01/solution.exs
+++ 2023/01/solution.exs
@@ -11,11 +11,8 @@ defmodule Day1.Main do
 
   defp parse_char(_, {first, last}), do: {first, last}
 
-  def part1(file) do
-    {:ok, file} = File.read(file)
-
-    file
-    |> String.split("\n")
+  def part1(lines) do
+    lines
     |> Enum.map(fn str ->
       {first, last} =
         str
@@ -44,40 +41,45 @@ defmodule Day1.Main do
   defp starts_with([h1 | t1], [h2 | t2]) when h1 == h2, do: starts_with(t1, t2)
   defp starts_with(_, _), do: false
 
-  def parse_line(str) do
-    parse_line_recursive(-1, -1, str |> String.to_charlist())
-  end
-
-  defp parse_line_recursive(first, last, []) do
+  defp parse_line(first, last, []) do
     first * 10 + last
   end
 
-  defp parse_line_recursive(-1, _last, [c | rest]) when ?0 <= c and c <= ?9 do
+  defp parse_line(-1, _last, [c | rest]) when ?0 <= c and c <= ?9 do
     n = c - ?0
-    parse_line_recursive(n, n, rest)
+    parse_line(n, n, rest)
   end
 
-  defp parse_line_recursive(first, _last, [c | rest]) when ?0 <= c and c <= ?9 do
+  defp parse_line(first, _last, [c | rest]) when ?0 <= c and c <= ?9 do
     n = c - ?0
-    parse_line_recursive(first, n, rest)
+    parse_line(first, n, rest)
   end
 
-  defp parse_line_recursive(first, last, str) do
-    [_ | rest] = str
+  defp parse_line(first, last, [_ | rest]) do
+    word = @mappings |> Enum.find(fn {k, _} -> k end)
 
-    case {first, @mappings |> Enum.find(fn {k, _} -> starts_with(str, k) end)} do
-      {_, nil} -> parse_line_recursive(first, last, rest)
-      {-1, {_, v}} -> parse_line_recursive(v, v, rest)
-      {first, {_, v}} -> parse_line_recursive(first, v, rest)
+    case {first, word} do
+      {_, nil} -> parse_line(first, last, rest)
+      {-1, {_, v}} -> parse_line(v, v, rest)
+      {first, {_, v}} -> parse_line(first, v, rest)
     end
   end
 
-  def part2(file) do
-    {:ok, file} = File.read(file)
+  def parse_line(str) do
+    parse_line(-1, -1, str |> String.to_charlist())
+  end
 
-    file
-    |> String.split("\n")
+  def part2(lines) do
+    lines
     |> Enum.map(&parse_line/1)
     |> Enum.sum()
   end
 end
+
+lines = IO.read(:stdio, :eof) |> String.split("\n")
+
+IO.write("Part 1:")
+lines |> Day1.Main.part1() |> IO.inspect()
+
+IO.write("Part 2:")
+lines |> Day1.Main.part2() |> IO.inspect()
