229e6a3e236f83e444a69cafe3f4383dfe7a580c
Einar Norðfjörð <gloogankle@gmail.com>
2023-12-07 01:47:21 -0500

    cleanup

--- 2023/07/solution.ml
+++ 2023/07/solution.ml
@@ -12,41 +12,26 @@ module Hand = struct
     | 'A' -> 14
     | 'K' -> 13
     | 'Q' -> 12
-    | 'J' -> -1
+    | 'J' -> 11
     | 'T' -> 10
     | c -> int_of_char c - int_of_char '0'
 
-  let to_char = function
-    | 14 -> 'A'
-    | 13 -> 'K'
-    | 12 -> 'Q'
-    | -1 -> 'J'
-    | 10 -> 'T'
-    | c -> char_of_int (c + int_of_char '0')
-
   let of_string s = String.to_seq s |> Seq.map of_char |> List.of_seq
-  let to_chars = Array.map to_char
-  let to_string h = h |> List.map to_char |> List.to_seq |> String.of_seq
 
-  let rank hand =
-    let no_jacks = hand |> List.filter (fun x -> x <> -1) in
-    let jacks = hand |> List.filter (fun x -> x = -1) |> List.length in
-    let cards =
-      no_jacks
-      |> List.sort compare
-      |> List.to_seq
-      |> Seq.group ( = )
-      |> Seq.map Seq.length
-      |> List.of_seq
-      |> List.sort (fun a b -> -compare a b)
-    in
-    let cards =
-      match cards with
-      | [] -> [ jacks ]
-      | [ x ] -> [ x + jacks ]
-      | x :: xs -> (x + jacks) :: xs
-    in
-    match cards with
+  let compare = compare
+end
+
+module Part1 = struct
+  let group_by_card hand =
+    hand
+    |> List.sort compare
+    |> List.to_seq
+    |> Seq.group ( = )
+    |> Seq.map Seq.length
+    |> List.of_seq
+    |> List.sort (fun a b -> b - a)
+
+  let rank_grouped = function
     | [ 5 ] -> 6
     | [ 4; _ ] -> 5
     | [ 3; 2 ] -> 4
@@ -55,18 +40,33 @@ module Hand = struct
     | 2 :: _ -> 1
     | _ -> 0
 
-  let rec compare_hand h1 h2 =
-    match (h1, h2) with
-    | [], [] -> 0
-    | a :: _, b :: _ when a > b -> 1
-    | a :: _, b :: _ when a < b -> -1
-    | _ :: xs, _ :: ys -> compare_hand xs ys
-    | _ -> failwith "impossible"
+  let rank hand = group_by_card hand |> rank_grouped
 
   let compare hand1 hand2 =
     let rank1 = rank hand1 in
     let rank2 = rank hand2 in
-    if rank1 > rank2 then 1 else if rank1 < rank2 then -1 else compare_hand hand1 hand2
+    if rank1 > rank2 then 1 else if rank1 < rank2 then -1 else Hand.compare hand1 hand2
+end
+
+module Part2 = struct
+  type t = int list [@@deriving show]
+
+  let transform_jacks = List.map (fun x -> if x = 11 then -1 else x)
+
+  let add_jacks jacks = function
+    | [] -> [ jacks ]
+    | x :: xs -> (x + jacks) :: xs
+
+  let rank hand =
+    let jacks, no_jacks = hand |> List.partition (fun x -> x = -1) in
+    no_jacks |> Part1.group_by_card |> add_jacks (List.length jacks) |> Part1.rank_grouped
+
+  let compare hand1 hand2 =
+    let hand1 = transform_jacks hand1 in
+    let hand2 = transform_jacks hand2 in
+    let rank1 = rank hand1 in
+    let rank2 = rank hand2 in
+    if rank1 > rank2 then 1 else if rank1 < rank2 then -1 else Hand.compare hand1 hand2
 end
 
 let parse line =
@@ -77,23 +77,21 @@ let parse line =
 type parsed = Hand.t * int [@@deriving show]
 
 let hands = List.map parse lines
-let ( >> ) f g x = g (f x)
-
-let tap f x =
-  f x;
-  x
 
 let part1 () =
   hands
-  |> List.sort (fun (h1, _) (h2, _) -> Hand.compare h1 h2)
-  |> List.mapi (fun i x -> (i + 1, x))
-  |> List.fold_left
-       (fun acc (i, (h, bid)) ->
-         Printf.printf "i=%d; h=%s; r=%d; b=%d\n" i (Hand.to_string h) (Hand.rank h) bid;
-         acc + (i * bid))
-       0
+  |> List.sort (fun (h1, _) (h2, _) -> Part1.compare h1 h2)
+  |> List.mapi (fun i x -> (i + 1, snd x))
+  |> List.fold_left (fun acc (i, bid) -> acc + (i * bid)) 0
   |> Printf.printf "Part 1: %d\n"
+
+let part2 () =
+  hands
+  |> List.sort (fun (h1, _) (h2, _) -> Part2.compare h1 h2)
+  |> List.mapi (fun i x -> (i + 1, snd x))
+  |> List.fold_left (fun acc (i, bid) -> acc + (i * bid)) 0
+  |> Printf.printf "Part 2: %d\n"
 
 let () =
   part1 ();
-  "QQQJQ" |> Hand.of_string |> Hand.rank |> Printf.printf "%d\n"
+  part2 ()
