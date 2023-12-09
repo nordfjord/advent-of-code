f75dbadc36505fe27e96c199a7f19a41e4e35f23
Einar Norðfjörð <gloogankle@gmail.com>
2023-12-09 01:13:31 -0500

    2023 day 9

---
+++ 2023/09/solution.ml
@@ -0,0 +1,42 @@
+let lines =
+  Seq.of_dispenser (fun _ ->
+    match read_line () with
+    | x -> Some x
+    | exception End_of_file -> None)
+  |> List.of_seq
+
+let parse line = String.split_on_char ' ' line |> List.map int_of_string
+
+let rec pairwise = function
+  | x :: y :: tl -> (x, y) :: pairwise (y :: tl)
+  | _ -> []
+
+let extrapolate_one_level list = pairwise list |> List.map (fun (x, y) -> y - x)
+
+let extrapolate list =
+  let rec aux l list =
+    let next = extrapolate_one_level list in
+    if List.for_all (fun x -> x = 0) next
+    then next :: l
+    else aux (next :: l) next
+  in
+  aux [ list ] list
+
+let last l = List.rev l |> List.hd
+let determine_next = List.fold_left (fun acc xs -> acc + last xs) 0
+
+let () =
+  let extrapolated = lines |> List.map parse |> List.map extrapolate in
+  extrapolated
+  |> List.map determine_next
+  |> List.fold_left ( + ) 0
+  |> Printf.printf "Part 1: %d\n"
+
+let determine_previous = List.fold_left (fun acc xs -> List.hd xs - acc) 0
+
+let () =
+  let extrapolated = lines |> List.map parse |> List.map extrapolate in
+  extrapolated
+  |> List.map determine_previous
+  |> List.fold_left ( + ) 0
+  |> Printf.printf "Part 2: %d\n"
