6b2115ebe49bf92957713db0d5c463c9679e4463
Einar Norðfjörð <gloogankle@gmail.com>
2023-12-13 01:30:43 -0500

    cleanup

--- 2023/13/solution.ml
+++ 2023/13/solution.ml
@@ -7,58 +7,75 @@ let grids =
   |> List.map ~f:(fun s ->
     s |> String.split_lines |> List.map ~f:String.to_array |> Array.of_list)
 
-let smudge_eq a1 a2 =
-  let rec aux count i =
-    if count > 1
-    then `not_eq
-    else if i = Array.length a1
-    then if count = 1 then `smudge else `eq
-    else if Char.equal a1.(i) a2.(i)
-    then aux count (i + 1)
-    else aux (count + 1) (i + 1)
-  in
-  aux 0 0
+module Part1 = struct
+  let eq = Array.equal Char.equal
 
-let validate_reflection arr i =
-  let rec aux i j smudges =
-    let out_of_bounds = i < 0 || j >= Array.length arr in
-    if smudges > 1
-    then false
-    else if out_of_bounds
-    then if smudges = 1 then true else false
-    else (
-      match smudge_eq arr.(i) arr.(j) with
-      | `smudge -> aux (i - 1) (j + 1) (smudges + 1)
-      | `eq -> aux (i - 1) (j + 1) smudges
-      | `not_eq -> false)
-  in
-  aux i (i + 1) 0
+  let is_reflected_at arr i =
+    let rec aux i j =
+      if i < 0 || j >= Array.length arr
+      then true
+      else if eq arr.(i) arr.(j)
+      then aux (i - 1) (j + 1)
+      else false
+    in
+    if i = Array.length arr - 1 then false else aux i (i + 1)
+end
 
-let find_reflection arr =
+module Part2 = struct
+  type eq =
+    | Smudge
+    | Equal
+    | NotEqual
+
+  let smudge_eq a1 a2 =
+    let rec aux count i =
+      if count > 1
+      then NotEqual
+      else if i = Array.length a1
+      then if count = 1 then Smudge else Equal
+      else if Char.equal a1.(i) a2.(i)
+      then aux count (i + 1)
+      else aux (count + 1) (i + 1)
+    in
+    aux 0 0
+
+  let is_reflected_at arr i =
+    let rec aux i j smudges =
+      if smudges > 1
+      then false
+      else if i < 0 || j >= Array.length arr
+      then if smudges = 1 then true else false
+      else (
+        match smudge_eq arr.(i) arr.(j) with
+        | Smudge -> aux (i - 1) (j + 1) (smudges + 1)
+        | Equal -> aux (i - 1) (j + 1) smudges
+        | NotEqual -> false)
+    in
+    aux i (i + 1) 0
+end
+
+let find_reflection is_reflected_at arr =
   let rec aux i =
-    if i = Array.length arr - 1
+    if i = Array.length arr
     then None
-    else if validate_reflection arr i
-    then Some i
+    else if is_reflected_at arr i
+    then Some (i + 1)
     else aux (i + 1)
   in
   aux 0
 
-let print grid =
-  Array.mapi grid ~f:(fun i s -> Printf.sprintf "%2d %s" i (String.of_array s))
-  |> String.concat_array ~sep:"\n"
-  |> print_endline
-
-let solve arr =
+let solve find_reflection arr =
   match find_reflection arr with
-  | Some i -> (i + 1) * 100
+  | Some i -> i * 100
   | None ->
     (match find_reflection (Array.transpose_exn arr) with
-     | Some i -> i + 1
-     | None ->
-       print arr;
-       printf "\n";
-       print (Array.transpose_exn arr);
-       failwith "No reflection")
+     | Some i -> i
+     | None -> failwith "No reflection")
 
-let () = grids |> List.sum (module Int) ~f:solve |> printf "%d\n"
+let () =
+  grids
+  |> List.sum (module Int) ~f:(solve (find_reflection Part1.is_reflected_at))
+  |> printf "%d\n";
+  grids
+  |> List.sum (module Int) ~f:(solve (find_reflection Part2.is_reflected_at))
+  |> printf "%d\n"
