5acc833ea2aef75384b5c0c7207779013d6585b6
Einar Norðfjörð <gloogankle@gmail.com>
2023-12-13 01:09:35 -0500

    2023 day 13 part 2

--- 2023/13/solution.ml
+++ 2023/13/solution.ml
@@ -7,41 +7,58 @@ let grids =
   |> List.map ~f:(fun s ->
     s |> String.split_lines |> List.map ~f:String.to_array |> Array.of_list)
 
-let eq = Array.equal Char.equal
+let smudge_eq a1 a2 =
+  let rec aux count i =
+    if count > 1
+    then `not_eq
+    else if i = Array.length a1
+    then if count = 1 then `smudge else `eq
+    else if Char.equal a1.(i) a2.(i)
+    then aux count (i + 1)
+    else aux (count + 1) (i + 1)
+  in
+  aux 0 0
 
-let rec extrapolate_reflection arr i j =
-  printf "i=%d; j=%d\n" i j;
-  if i < 0 || j >= Array.length arr
-  then true
-  else if eq arr.(i) arr.(j)
-  then extrapolate_reflection arr (i - 1) (j + 1)
-  else false
+let validate_reflection arr i =
+  let rec aux i j smudges =
+    let out_of_bounds = i < 0 || j >= Array.length arr in
+    if smudges > 1
+    then false
+    else if out_of_bounds
+    then if smudges = 1 then true else false
+    else (
+      match smudge_eq arr.(i) arr.(j) with
+      | `smudge -> aux (i - 1) (j + 1) (smudges + 1)
+      | `eq -> aux (i - 1) (j + 1) smudges
+      | `not_eq -> false)
+  in
+  aux i (i + 1) 0
 
 let find_reflection arr =
   let rec aux i =
     if i = Array.length arr - 1
     then None
-    else if eq arr.(i) arr.(i + 1) && extrapolate_reflection arr i (i + 1)
+    else if validate_reflection arr i
     then Some i
     else aux (i + 1)
   in
   aux 0
 
 let print grid =
-  Array.map grid ~f:String.of_array |> String.concat_array ~sep:"\n" |> print_endline
+  Array.mapi grid ~f:(fun i s -> Printf.sprintf "%2d %s" i (String.of_array s))
+  |> String.concat_array ~sep:"\n"
+  |> print_endline
 
 let solve arr =
   match find_reflection arr with
-  | Some i ->
-    printf "row %d\n" i;
-    (i + 1) * 100
+  | Some i -> (i + 1) * 100
   | None ->
     (match find_reflection (Array.transpose_exn arr) with
-     | Some i ->
-       printf "col %d\n" i;
-       i + 1
+     | Some i -> i + 1
      | None ->
        print arr;
+       printf "\n";
+       print (Array.transpose_exn arr);
        failwith "No reflection")
 
 let () = grids |> List.sum (module Int) ~f:solve |> printf "%d\n"
