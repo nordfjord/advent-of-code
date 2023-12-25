d37e73eefd40de398c12b273b78236c76a479bd3
Einar Norðfjörð <gloogankle@gmail.com>
2023-12-24 23:03:54 -0500

    2023 day 24 part 2 ocaml

--- 2023/24/solution.ml
+++ 2023/24/solution.ml
@@ -2,25 +2,19 @@ open Base
 open Stdio
 
 module Point3D = struct
-  type t = float * float * float [@@deriving compare, sexp, hash, equal]
+  type t = int * int * int [@@deriving compare, sexp, hash, equal]
 
-  let add (x1, y1, z1) (x2, y2, z2) = Float.(x1 + x2, y1 + y2, z1 + z2)
-  let to_2d (x, y, _) = (x, y)
-  let sub (x1, y1, z1) (x2, y2, z2) = (x1 - x2, y1 - y2, z1 - z2)
+  let to_2d (x, y, _) = (Float.of_int x, Float.of_int y)
 end
 
 module Point2D = struct
   type t = float * float [@@deriving compare, sexp, hash, equal]
 
-  let add (x1, y1) (x2, y2) = Float.(x1 + x2, y1 + y2)
-  let ( + ) = add
-
-  let ( > ) (x1, y1) (x2, y2) =
-    if Float.(x1 > x2) then true else if Float.(x1 < x2) then false else Float.(y1 > y2)
-
   let sub (x1, y1) (x2, y2) = Float.(x1 - x2, y1 - y2)
-  let of_3d (x, y, _) = (x, y)
-  let cross_product (x1, y1) (x2, y2) = Float.((x1 * y2) - (y1 * x2))
+
+  let cross_product (x1, y1) (x2, y2) =
+    let open Float in
+    (x1 * y2) - (y1 * x2)
 end
 
 module Line = struct
@@ -55,7 +49,7 @@ end
 let parse_coordinates str =
   Str.split (Str.regexp_string ", ") str
   |> List.map ~f:String.strip
-  |> List.map ~f:Float.of_string
+  |> List.map ~f:Int.of_string
   |> function
   | [ x; y; z ] -> (x, y, z)
   | _ -> failwith "invalid coordinates"
@@ -90,3 +84,34 @@ let () =
       Float.(x >= area_min && x <= area_max && y >= area_min && y <= area_max))
   in
   printf "%d\n" counts
+
+let () =
+  let open Z3 in
+  let ctx = mk_context [] in
+  let mk_int_c x = Arithmetic.Integer.mk_numeral_i ctx x in
+  let mk_int name = Arithmetic.Integer.mk_const_s ctx name in
+  let x = mk_int "x" in
+  let y = mk_int "y" in
+  let z = mk_int "z" in
+  let vx = mk_int "vx" in
+  let vy = mk_int "vy" in
+  let vz = mk_int "vz" in
+  let solver = Solver.mk_solver ctx None in
+  List.iteri points ~f:(fun i { point = xi, yi, zi; direction = vxi, vyi, vzi } ->
+    let t = mk_int (Printf.sprintf "t%d" i) in
+    let mk_formula x vx xi vxi =
+      Boolean.mk_eq
+        ctx
+        (Arithmetic.mk_add ctx [ x; Arithmetic.mk_mul ctx [ t; vx ] ])
+        (Arithmetic.mk_add ctx [ mk_int_c xi; Arithmetic.mk_mul ctx [ t; mk_int_c vxi ] ])
+    in
+    Solver.add
+      solver
+      [ mk_formula x vx xi vxi; mk_formula y vy yi vyi; mk_formula z vz zi vzi ]);
+  let _ = Solver.check solver [] in
+  let model = Solver.get_model solver |> Option.value_exn in
+  let sum = Arithmetic.mk_add ctx [ x; y; z ] in
+  let (Some sum) = Model.eval model sum true in
+  let sum = Arithmetic.Integer.get_big_int sum in
+  printf "%s\n" (Z.to_string sum)
+[@@warning "-8"]
