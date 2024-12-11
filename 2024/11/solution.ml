5f058d5eb2dbd0bee3364a5900078e3b40220a4b
Einar Norðfjörð <gloogankle@gmail.com>
2024-12-11 00:55:23 -0500

    day 11

---
+++ 2024/11/solution.ml
@@ -0,0 +1,47 @@
+open Base
+open Stdio
+
+let set_or_add x = function
+  | None -> x
+  | Some y -> x + y
+
+let line =
+  In_channel.input_all stdin
+  |> String.split ~on:' '
+  |> List.fold
+       ~init:(Hashtbl.create (module Int))
+       ~f:(fun map x ->
+         Hashtbl.update map (Int.of_string x) ~f:(set_or_add 1);
+         map)
+
+let rec num_digits n = if n = 0 then 0 else 1 + num_digits (n / 10)
+
+let split_number n =
+  let half = num_digits n / 2 in
+  let divider = 10 ** half in
+  (n / divider, n % divider)
+
+let blink map =
+  let next = Hashtbl.create (module Int) in
+  Hashtbl.iteri map ~f:(fun ~key ~data:count ->
+    let digits = num_digits key in
+    if key = 0
+    then Hashtbl.update next 1 ~f:(set_or_add count)
+    else if digits % 2 = 0
+    then (
+      let left, right = split_number key in
+      Hashtbl.update next left ~f:(set_or_add count);
+      Hashtbl.update next right ~f:(set_or_add count))
+    else Hashtbl.update next (key * 2024) ~f:(set_or_add count));
+  next
+
+let rec simulate map n =
+  if n = 0
+  then Hashtbl.fold map ~init:0 ~f:(fun ~key:_ ~data sum -> sum + data)
+  else simulate (blink map) (n - 1)
+
+type intlist = int list [@@deriving sexp]
+
+let () =
+  simulate line 25 |> printf "Part 1: %d\n";
+  simulate line 75 |> printf "Part 2: %d\n"
