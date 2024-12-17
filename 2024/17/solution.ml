a1e90f0b3fe8385ee044e4d78ad939ba88d520f7
Einar Norðfjörð <gloogankle@gmail.com>
2024-12-17 13:55:50 -0500

    day 17

--- 2024/17/solution.ml
+++ 2024/17/solution.ml
@@ -14,7 +14,7 @@ let part1 a =
     let b = calc_b a in
     let a = a / 8 in
     let out = (b % 8) :: out in
-    if a = 0 then List.rev out |> List.to_array else aux a out
+    if a = 0 then List.rev out else aux a out
   in
   aux a []
 
@@ -30,25 +30,21 @@ let print_out out =
 let part2 () =
   let rec solve potential_as i =
     if i < 0
-    then List.hd_exn potential_as
+    then Sequence.hd_exn potential_as
     else (
       let next =
-        List.fold potential_as ~init:[] ~f:(fun acc a ->
+        Sequence.concat_map potential_as ~f:(fun a ->
           let a = a * 8 in
-          let new_as =
-            Sequence.range a (a + 7)
-            |> Sequence.filter_map ~f:(fun a ->
-              if calc_b a = expected.(i) then Some a else None)
-            |> Sequence.to_list
-          in
-          List.append acc new_as)
+          Sequence.range a (a + 7)
+          |> Sequence.filter ~f:(fun a -> calc_b a = expected.(i)))
       in
       solve next (i - 1))
   in
-  solve [ 0 ] 15
+  solve (Sequence.singleton 0) 15
 
 let () =
-  let p1 = part1 61156655 in
-  printf "Part1: ";
-  print_out p1;
+  part1 61156655
+  |> List.map ~f:Int.to_string
+  |> String.concat ~sep:","
+  |> printf "Part1: %s\n";
   part2 () |> printf "Part 2: %d\n"
