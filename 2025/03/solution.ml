49609415801c43d148dbe5e70fe605315f1f59e4
Einar Norðfjörð <gloogankle@gmail.com>
2025-12-04 09:50:55 -0500

    pretty day 3

--- 2025/03/solution.ml
+++ 2025/03/solution.ml
@@ -6,23 +6,28 @@ let lines = In_channel.input_lines stdin
 let parse str =
   str |> String.to_array |> Array.map ~f:(fun x -> Char.to_int x - Char.to_int '0')
 
-let solve k lst =
-  let digits = Array.create ~len:k 0 in
-  let rec aux (idx, v) sidx i arr k =
-    let max_idx = Array.length arr - (k - 1) in
-    if i >= Array.length digits
-    then ()
-    else if sidx >= max_idx
-    then (
-      digits.(i) <- v;
-      aux (0, 0) (idx + 1) (i + 1) arr (k - 1))
-    else if arr.(sidx) > v
-    then aux (sidx, arr.(sidx)) (sidx + 1) i arr k
-    else aux (idx, v) (sidx + 1) i arr k
+let solve k arr =
+  let n = Array.length arr in
+  let find_max_in_range start end_pos =
+    let rec aux i max_idx =
+      if i > end_pos
+      then max_idx
+      else if arr.(i) > arr.(max_idx)
+      then aux (i + 1) i
+      else aux (i + 1) max_idx
+    in
+    aux (start + 1) start
   in
-  aux (0, 0) 0 0 lst k;
-  let num = Array.fold digits ~init:0 ~f:(fun acc x -> (acc * 10) + x) in
-  num
+  let rec max_joltage i next_start acc =
+    if i >= k
+    then acc
+    else (
+      let remaining = k - i - 1 in
+      let max_end = n - remaining - 1 in
+      let max_idx = find_max_in_range next_start max_end in
+      max_joltage (i + 1) (max_idx + 1) ((acc * 10) + arr.(max_idx)))
+  in
+  max_joltage 0 0 0
 
 let () =
   let batteries = List.map lines ~f:parse in
